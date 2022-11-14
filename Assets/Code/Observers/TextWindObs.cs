e8d1f87876535307409391009ca9ba84b0d53241
RamiShehadeh <ramishehadeh13@gmail.com>
2022-11-14 17:08:03 -0500

    textwindobs

--- Assets/Code/Observers/TextWindObs.cs
+++ Assets/Code/Observers/TextWindObs.cs
@@ -18,6 +18,25 @@ namespace Wx
         void ReportWind(float d, float s)
         {
             // Add code to manage reports of state changes.
+            if (d > 157.5f && d <= 202.5f)
+                tmp.text = "S \n" + s;
+            if (d > 202.5f && d <= 247.5f)
+                tmp.text = "SW \n" + s;
+            if (d > 247.5f && d <= 292.5f)
+                tmp.text = "W \n" + s;
+            if (d > 292.5f && d <= 337.5f)
+                tmp.text = "NW \n" + s;
+            if (d > 337.5f && d <= 360f)
+                tmp.text = "N \n" + s;
+            if (d >= 0f && d <= 22.5f)
+                tmp.text = "N \n" + s;
+            if (d > 22.5f && d <= 67.5f)
+                tmp.text = "NE \n" + s;
+            if (d > 67.5f && d <= 112.5f)
+                tmp.text = "E \n" + s;
+            if (d > 112.5f && d <= 157.5f)
+                tmp.text = "SE \n" + s;
+
         }
     }
 }
